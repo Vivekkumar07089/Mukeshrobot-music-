aa634b276131d5d0138aebcabe7dfefbd92dc841
Vivek kumar <155367948+Vivekkumar07089@users.noreply.github.com>
2024-03-07 11:53:14 +0530

    Update bot

---
+++ VIPMUSIC/plugins/bot/botinfo.py
@@ -0,0 +1,23 @@
+import requests
+
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+BOT_TOKEN = 'YOUR_BOT_TOKEN'
+
+# Telegram API endpoint for getting information about the bot
+API_URL = f'https://api.telegram.org/bot{BOT_TOKEN}/getMe'
+
+# Make a request to the Telegram API
+response = requests.get(API_URL)
+data = response.json()
+
+# Extract relevant information
+bot_id = data['result']['id']
+bot_name = data['result']['first_name']
+bot_username = data['result']['username']
+bot_mention = f'@{bot_username}'
+
+# Print the obtained information
+print(f'Bot ID: {bot_id}')
+print(f'Bot Name: {bot_name}')
+print(f'Bot Username: {bot_username}')
+print(f'Bot Mention: {bot_mention}')
