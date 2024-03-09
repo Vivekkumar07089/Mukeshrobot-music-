50aec869b2af2c0c5bb10fd26ae29317603bb15d
Vivek kumar <155367948+Vivekkumar07089@users.noreply.github.com>
2024-03-09 10:44:21 +0530

    Update randomreaction.py

--- VIPMUSIC/plugins/tools/randomreaction.py
+++ VIPMUSIC/plugins/tools/randomreaction.py
@@ -1,5 +1,4 @@
 import random
-from pyrogram.types import Message
 from pyrogram import Client, filters
 from VIPMUSIC import app
 from VIPMUSIC.core.userbot import Userbot
@@ -7,15 +6,12 @@ from VIPMUSIC.utils.database import get_assistant
 
 reaction_list = ["🔥", "😊", "👍", "💬", "🎉"]  # Add your desired reactions
 
-@app.on_message(filters.private | filters.group)
-async def send_random_reaction(client, message):
-    if "/" in message.text:
-        random_reaction = random.choice(reaction_list)
-        await app.send_reaction(message.chat.id, message.message_id, random_reaction)
 
 @app.on_message(filters.private | filters.group)
-async def send_v_random_reaction(client, message):
+async def send_v_random_reaction(_, message):
     if "/" in message.text:
         userbot = await get_assistant(message.chat.id)
+        chat_id = message.chat.id
+
         random_reaction = random.choice(reaction_list)
-        await userbot.send_reaction(message.chat.id, message.message_id, random_reaction)
+        await userbot.send_reaction(chat_id, message.message_id, random_reaction)
